a434348d876504d7737592550b5b26f1c0a7bbf6
Jinook <jinook0707@gmail.com>
2019-09-12 11:24:22 +0200

    Update eca.py

--- eca.py
+++ eca.py
@@ -1,10 +1,32 @@
+# coding: UTF-8
 """ Elementary cellular automata with Wolfram code.
 
 This program was coded and tested on Mac OSX 10.13.
 
+Jinook Oh, Cognitive Biology department, University of Vienna
+September 2019.
+
 Dependency:
     wxPython (4.0)
     Numpy (1.17)
+
+------------------------------------------------------------------------
+Copyright (C) 2019 Jinook Oh, W. Tecumseh Fitch 
+- Contact: jinook.oh@univie.ac.at, tecumseh.fitch@univie.ac.at
+
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your 
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but 
+WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along 
+with this program.  If not, see <http://www.gnu.org/licenses/>.
+------------------------------------------------------------------------
 """
 
 import sys, queue
